0bd58b6ca93cd7f7a3a32fd78949ebd6e9c37e9e
lajawanti <lajawanti.dhake@gmail.com>
2021-02-24 17:49:48 -0500

    product detail component

--- src/components/productDetail/ProductDetail.js
+++ src/components/productDetail/ProductDetail.js
@@ -17,24 +17,48 @@ const ProductDetail = (id) => {
             <div className = "productdetail_main">
                   <h1 style = {{color:'rgb(2, 109, 109)', fontSize:'40px'}}>boys</h1>
                   <div className="productdetail_all_info">
-                        <div className = "productdetail_image"style = {{  border: '1px solid yellow'}}>
+                        <div className = "productdetail_image">
                               <img src = {productLocal.image} alt = "product_image"/>
                         </div>
 
-                        <div className = "productdetail_text" style = {{  border: '1px solid green'}}>
+                        <div className = "productdetail_text">
                               <div className = "productdetail_title_price">
                                     <p style = {{width: '250px'}}>{productLocal.title}</p>
                                     <p>${productLocal.price}</p>
                               </div>
                               <div className = "productdetail_size_color">
-                                    <p>size <span style = {{color:'rgb(2, 109, 109)'}}>: size chart</span></p>
-                                    <p>color : required</p>
+                                    <div className = "size-radio-buttons">
+                                          <p style = {{paddingBottom:'10px'}}>size <span style = {{color:'rgb(2, 109, 109)'}}>: size chart</span></p>
+                                          <input type="radio" name="size" value="0-12 months"/>0-12 months<br/>
+                                          <input type="radio" name="size" value="12-24 months"/>12-24 months<br/>
+                                          <input type="radio" name="size" value="2-5 years"/>2-5 years<br/>
+                                          <input type="radio" name="size" value="6-12 years"/>6-12 years<br/>
+                                    </div>
+                                    <div className = "size-radio-buttons">
+                                          <p style = {{paddingBottom:'10px'}}>color : required</p>
+                                          <input type="radio" name="color" value="0-12 months"/>Teal<br/>
+                                          <input type="radio" name="color" value="12-24 months"/>White<br/>
+                                          <input type="radio" name="color" value="2-5 years"/>Green<br/>
+                                          <input type="radio" name="color" value="6-12 years"/>Blue<br/>
+                                    </div>
                               </div>
                               <div className = "productdetail_quantity">
                                     <span>quantity: &nbsp; <button>-</button>&nbsp; &nbsp;1&nbsp; &nbsp;<button>+</button></span>                                    
                               </div>
                               <button className = "add_to_cart">add to cart</button>
                         </div>
+                  </div>
+
+                  <div className = "productdetail_description">
+                        <p style = {{marginBottom: '20px', fontSize:'17px'}}>Description</p>
+
+                        <p>Your 1970s gym class is calling! Retro inspired velour short with our signature Avocado Print pairs with anything, really!</p>
+                        <p>- Machine Wash Cold/Low Heat or Flat Dry</p>
+                        <p>By Angel Dear</p>
+                  </div>
+
+                  <div className = "productdetail_related_products">
+                        <p style = {{marginBottom: '20px', fontSize:'17px'}}>Related Products</p>
                   </div>
             </div>
       )
